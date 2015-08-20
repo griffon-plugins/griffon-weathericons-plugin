bd8f0dc665e40cc2ddfebb703976bd981171b215
aalmiray <aalmiray@gmail.com>
2015-08-20 17:24:50 +0200

    Enable JavaFX properties on JavaFX package

--- subprojects/griffon-weathericons-javafx/src/main/java/griffon/javafx/support/weathericons/WeatherIcon.java
+++ subprojects/griffon-weathericons-javafx/src/main/java/griffon/javafx/support/weathericons/WeatherIcon.java
@@ -17,7 +17,14 @@ package griffon.javafx.support.weathericons;
 
 import griffon.core.editors.PropertyEditorResolver;
 import griffon.plugins.weathericons.WeatherIconFont;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
@@ -34,7 +41,7 @@ import static java.util.Objects.requireNonNull;
  */
 public class WeatherIcon extends Text {
     private static final String WEATHERICON_SET = "META-INF/resources/weathericons/2.0.1/fonts/weathericons-regular-webfont.ttf";
-    private static final String ERROR_FONT_WEATHERICON_NULL = "Argument 'weatherIcon' must not be null";
+    private static final String ERROR_WEATHERICON_NULL = "Argument 'weatherIcon' must not be null";
 
     private static final String WEATHERICON_FONT_FAMILY;
 
@@ -43,16 +50,30 @@ public class WeatherIcon extends Text {
         WEATHERICON_FONT_FAMILY = font.getFamily();
     }
 
-    private WeatherIconFont weatherIcon;
-    private int iconSize;
-    private Color iconColor;
+    private ObjectProperty<WeatherIconFont> weatherIcon;
+    private IntegerProperty iconSize;
+    private ObjectProperty<Paint> iconColor;
+
+    private ChangeListener<Number> iconSizeChangeListener = new ChangeListener<Number>() {
+        @Override
+        public void changed(ObservableValue<? extends Number> v, Number o, Number n) {
+            setStyle(getStyle() + " -fx-font-size: " + n + "px;");
+        }
+    };
+
+    private ChangeListener<Paint> iconColorChangeListener = new ChangeListener<Paint>() {
+        @Override
+        public void changed(ObservableValue<? extends Paint> v, Paint o, Paint n) {
+            setFill(n);
+        }
+    };
 
     public WeatherIcon() {
         this(WeatherIconFont.WI_WINDY);
     }
 
     public WeatherIcon(@Nonnull WeatherIconFont weatherIcon) {
-        this.weatherIcon = requireNonNull(weatherIcon, ERROR_FONT_WEATHERICON_NULL);
+        setWeatherIconFont(requireNonNull(weatherIcon, ERROR_WEATHERICON_NULL));
         getStyleClass().add("weathericons-icon");
         setText(String.valueOf(weatherIcon.getCode()));
         setStyle("-fx-font-family: '" + WEATHERICON_FONT_FAMILY + "';");
@@ -63,46 +84,78 @@ public class WeatherIcon extends Text {
     public WeatherIcon(@Nonnull String description) {
         this(WeatherIconFont.findByDescription(description));
         resolveSize(description);
-        resolveColor(description);
+        resolvePaint(description);
+    }
+
+    public ObjectProperty<WeatherIconFont> weatherIconProperty() {
+        if (weatherIcon == null) {
+            weatherIcon = new SimpleObjectProperty<>(this, "weatherIcon", null);
+        }
+        return weatherIcon;
+    }
+
+    public ObjectProperty<WeatherIconFont> getWeatherIconProperty() {
+        return weatherIconProperty();
+    }
+
+    public IntegerProperty iconSizeProperty() {
+        if (iconSize == null) {
+            iconSize = new SimpleIntegerProperty(this, "iconSize", 16);
+            iconSize.addListener(iconSizeChangeListener);
+        }
+        return iconSize;
+    }
+
+    public IntegerProperty getIconSizeProperty() {
+        return iconSizeProperty();
+    }
+
+    public ObjectProperty<Paint> iconColorProperty() {
+        if (iconColor == null) {
+            iconColor = new SimpleObjectProperty<>(this, "iconColor", null);
+            iconColor.addListener(iconColorChangeListener);
+        }
+        return iconColor;
+    }
+
+    public ObjectProperty<Paint> getIconColorProperty() {
+        return iconColorProperty();
     }
 
     @Nonnull
     public WeatherIconFont getWeatherIconFont() {
-        return weatherIcon;
+        return weatherIconProperty().get();
     }
 
-    public void setWeatherIcon(@Nonnull WeatherIconFont weatherIcon) {
-        this.weatherIcon = requireNonNull(weatherIcon, ERROR_FONT_WEATHERICON_NULL);
+    public void setWeatherIconFont(@Nonnull WeatherIconFont weatherIcon) {
+        weatherIconProperty().set(requireNonNull(weatherIcon, ERROR_WEATHERICON_NULL));
         setText(String.valueOf(weatherIcon.getCode()));
     }
 
-    public void setWeatherIcon(@Nonnull String description) {
+    public void setWeatherIconFont(@Nonnull String description) {
         requireNonBlank(description, "Argument 'description' must not be blank");
-        setWeatherIcon(WeatherIconFont.findByDescription(description));
+        weatherIconProperty().set(WeatherIconFont.findByDescription(description));
         resolveSize(description);
-        resolveColor(description);
+        resolvePaint(description);
     }
 
     public void setIconSize(int size) {
         requireState(size > 0, "Argument 'size' must be greater than zero.");
-        setStyle(getStyle() + " -fx-font-size: " + size + "px;");
-        this.iconSize = size;
+        iconSizeProperty().set(size);
     }
 
     public int getIconSize() {
-        return iconSize;
+        return iconSizeProperty().get();
     }
 
-
-    public void setIconColor(@Nonnull Color color) {
+    public void setIconColor(@Nonnull Paint color) {
         requireNonNull(color, "Argument 'color' must not be null");
-        setFill(color);
-        this.iconColor = color;
+        iconColorProperty().set(color);
     }
 
     @Nonnull
-    public Color getIconColor() {
-        return iconColor;
+    public Paint getIconColor() {
+        return iconColorProperty().get();
     }
 
     private void resolveSize(String description) {
@@ -118,14 +171,14 @@ public class WeatherIcon extends Text {
         }
     }
 
-    private void resolveColor(String description) {
+    private void resolvePaint(String description) {
         String[] parts = description.split(":");
         if (parts.length > 2) {
-            PropertyEditor editor = PropertyEditorResolver.findEditor(Color.class);
+            PropertyEditor editor = PropertyEditorResolver.findEditor(Paint.class);
             editor.setValue(parts[2]);
-            Color color = (Color) editor.getValue();
-            if (color != null) {
-                setIconColor(color);
+            Paint paint = (Paint) editor.getValue();
+            if (paint != null) {
+                setIconColor(paint);
             }
         }
     }
